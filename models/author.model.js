c37dd81cd5e66c57730da058b4dadfbf2ee9b62d
pb7932 <pebe1928@gmail.com>
2021-05-21 08:39:18 +0200

    Adding book and author models, and a book repository

---
+++ models/author.model.js
@@ -0,0 +1,24 @@
+module.exports = 
+    class Author {
+        constructor(id, firstName, lastName, DOB, gender) {
+            this.id = id;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.DOB = DOB;
+            this.gender = gender;
+        }
+        get age() {
+            return (new Date()).getFullYear() - this.DOB.getFullYear();
+        }
+        get formattedAuthor() {
+            return ((this.gender == 'm') ? 'Mr. ' : 'Mrs.') +
+                this.firstName +
+                ' ' + this.lastName +
+                ` (${this.age})`;
+        }
+    };
+
+AuthorClass.prototype.toString = function() {
+    return this.formattedAuthor;
+}
+module.exports = AuthorClass;
