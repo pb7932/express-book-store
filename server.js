cbcd3ffffa6586bff650c8f6c29dcbdfe911afc3
pb7932 <pebe1928@gmail.com>
2021-05-21 08:49:33 +0200

    Configuring a server, making necessary router files

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+app.use(function ( req, res, next ) ) {
+    console.log(req.url);
+    next();
+}
+
+const homeRouter = require('./routes/home.router');
+const booksRouter = require('./routes/books.router');
+const authorRouter = require('./routes/author.router');
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'views')));
+
+app.use('/', homeRouter);
+app.use('/books', booksRouter);
+app.use('/author', authorRouter);
+
+
+app.listen(80);
